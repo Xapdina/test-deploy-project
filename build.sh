6682ca34e7ec5d92e8422e2a935ae9ec81dfb7c5
Xapdina <daniilbagaturiya@ya.ru>
2025-02-18 09:36:16 +0300

    2

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# скачиваем uv
+# Скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
 
-# здесь добавьте все необходимые команды для установки вашего проекта
-# команду установки зависимостей, сборки статики, применения миграций и другие
+# Создаём виртуальное окружение
+uv venv
+
+# Активируем виртуальное окружение
+source .venv/bin/activate
+
+# Устанавливаем зависимости
 make install && make collectstatic && make migrate
